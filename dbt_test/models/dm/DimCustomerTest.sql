ecd7b3ffe000fed61cfaaa49f8d4e794cf4aba55
deshpcha <chaitanya.deshpande@tietoevry.com>
2021-07-29 12:00:55 +0530

    First check in
    first check in

---
+++ dbt_test/models/dm/DimCustomerTest.sql
@@ -0,0 +1,17 @@
+SELECT "CustomerKey" CustomerKey
+      ,"FirstName" FirstName
+      ,"MiddleName" MddleName
+      ,"LastName" LastName
+      ,"MaritalStatus" MaritalStatus
+      ,"Gender" Gender
+      ,"EmailAddress" EmailAddress
+      ,"YearlyIncome" Income
+      ,"TotalChildren" ChildrenCount
+      ,"NumberChildrenAtHome" ChildrenAtHome
+      ,"HouseOwnerFlag" HouseOwner
+      ,"NumberCarsOwned" CarsOwned
+      ,"AddressLine1" AddressLine1
+      ,"AddressLine2" AddressLine2
+      , '{invocation_ID}' as RunID
+     , 'CustomerOLTP' data_source_name
+  FROM {{source("demo_db","CUSTOMERSOURCE")}}  src 
